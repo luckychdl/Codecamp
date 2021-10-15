2b27579b390d527922ccb4d7db5c8d2c0a7fcf72
luckychdl <luckysksk@naver.com>
2021-10-15 19:31:26 +0900

    update

--- freeboard__frontend/src/commons/styles/globalStyles.ts
+++ freeboard__frontend/src/commons/styles/globalStyles.ts
@@ -5,11 +5,11 @@ export const globalStyles = css`
     margin: 0px;
     /* color: #616161; */
     /* box-sizing: border-box;*/
-    font-family: "Poor Story", cursive;
+    font-family: "Poor Story", "Arial", sans-serif;
   }
 
   @font-face {
-    font-family: "Poor Story", cursive;
+    font-family: "Poor Story", "Arial", sans-serif;
     src: url("https://fonts.googleapis.com/css2?family=Poor+Story&display=swap");
   }
 `;
